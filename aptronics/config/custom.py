99d404ae2c3db500e9309708514b18308975f5e5
Hemant Pema <hemant@pema.co.za>
2016-02-13 18:21:29 +0200

    update hooks and custom and erpnext wizard for apps with custom naming

--- aptronics/config/custom.py
+++ aptronics/config/custom.py
@@ -5,70 +5,104 @@ from frappe import _, msgprint, throw
 
 
 def get_custom_series(doc, method=None):
-    name = frappe.get_value(doc.doctype,doc.name,'name')
-    if name == None:
-        #frappe.msgprint("Insert")
+    try:
+        name = frappe.get_value(doc.doctype,doc.name,'name')
+        if name == None:
+            #frappe.msgprint("Insert")
+            prefix = get_prefix(doc)
+            series = get_series(doc, prefix)
+            doc.name=series
+            #doc.custom_series = series
+            #frappe.msgprint(series)
+            return doc
+        else:
+            pass
+    except:
         prefix = get_prefix(doc)
         series = get_series(doc, prefix)
         doc.name=series
-        #doc.custom_series = series
-        #frappe.msgprint(series)
         return doc
-    else:
-        #frappe.msgprint("name: " + name)
-        pass
 
 def get_prefix(doc):
     if doc.doctype == 'Customer':
         #frappe.msgprint(doc.customer_type)
         prefix = doc.customer_name[:3]
-        if doc.customer_type == 'Company':
-            #frappe.msgprint(doc.customer_name)
-            customerNameCount = frappe.db.sql("select count(`name`) as n from `tabCustomer` where customer_name = '" + doc.customer_name + "'")[0][0]
-            #frappe.msgprint(customerNameCount)
-            if customerNameCount >0:
-                frappe.throw(_("Duplicate company name."))
-        else:
-            customerNameCount = frappe.db.sql("select count(`name`) as n, tax_id from `tabCustomer` where customer_name = '" + doc.customer_name + "'", as_dict=1)[0]
-            #frappe.msgprint(customerNameCount)
-            if customerNameCount['n'] >0:
-                if doc.tax_id != None:
-                    if customerNameCount['tax_id'] != '' and customerNameCount['tax_id'] == doc.tax_id:
-                        frappe.throw(_("Duplicate company name."))
-                else:
-                    if customerNameCount['tax_id'] == '':
-                        frappe.throw(_("Duplicate company name."))
+        try:
+            if doc.customer_type == 'Company':
+                #frappe.msgprint(doc.customer_name)
+                customerNameCount = frappe.db.sql("select count(`name`) as n from `tabCustomer` where customer_name = '" + doc.customer_name + "'")[0][0]
+                #frappe.msgprint(customerNameCount)
+                if customerNameCount >0:
+                    frappe.throw(_("Duplicate company name."))
+            else:
+                customerNameCount = frappe.db.sql("select count(`name`) as n, tax_id from `tabCustomer` where customer_name = '" + doc.customer_name + "'", as_dict=1)[0]
+                #frappe.msgprint(customerNameCount)
+                if customerNameCount['n'] >0:
+                    if doc.tax_id != None:
+                        if customerNameCount['tax_id'] != '' and customerNameCount['tax_id'] == doc.tax_id:
+                            frappe.throw(_("Duplicate company name."))
+                    else:
+                        if customerNameCount['tax_id'] == '':
+                            frappe.throw(_("Duplicate company name."))
 
-        if doc.tax_id != None:
-            customerNameCount = frappe.db.sql("select count(`name`) as n from `tabCustomer` where tax_id = '" + doc.tax_id + "'")[0][0]
-            #frappe.msgprint(customerNameCount)
-            if customerNameCount >0:
-                frappe.throw(_("Duplicate company name with tax_id = " + doc.tax_id))
+            if doc.tax_id != None:
+                customerNameCount = frappe.db.sql("select count(`name`) as n from `tabCustomer` where tax_id = '" + doc.tax_id + "'")[0][0]
+                #frappe.msgprint(customerNameCount)
+                if customerNameCount >0:
+                    frappe.throw(_("Duplicate company name with tax_id = " + doc.tax_id))
+        except:
+            pass
 
     elif doc.doctype == 'Supplier':
         prefix = doc.supplier_name[:3]
-        supplierNameCount = frappe.db.sql("select count(`name`) as n from `tabSupplier` where supplier_name = '" + doc.supplier_name + "'")[0][0]
-        if supplierNameCount >0:
-           frappe.throw(_("Duplicate company name."))
+        try:
+            supplierNameCount = frappe.db.sql("select count(`name`) as n from `tabSupplier` where supplier_name = '" + doc.supplier_name + "'")[0][0]
+            if supplierNameCount >0:
+                frappe.throw(_("Duplicate company name."))
+        except:
+            pass
     elif doc.doctype == 'Contact':
         prefix = doc.first_name[:3]
-
     return prefix
 
 def get_series(doc, prefix):
-    countCustomer = frappe.db.sql("select count(`name`) as n from `tabCustomer` where customer_name like '" + prefix.upper() + "%'")[0][0]
-    countSupplier = frappe.db.sql("select count(`name`) as n from `tabSupplier` where supplier_name like '" + prefix.upper() + "%'")[0][0]
-    countContact = frappe.db.sql("select count(`name`) as n from `tabContact` where first_name like '" + prefix.upper() + "%'")[0][0]
+    try:
+        countCustomer = frappe.db.sql("select count(`name`) as n from `tabCustomer` where customer_name like '" + prefix.upper() + "%'")[0][0]
+    except:
+        countCustomer = 0
+
+    try:
+        countSupplier = frappe.db.sql("select count(`name`) as n from `tabSupplier` where supplier_name like '" + prefix.upper() + "%'")[0][0]
+    except:
+        countSupplier=0
+
+    try:
+        countContact = frappe.db.sql("select count(`name`) as n from `tabContact` where first_name like '" + prefix.upper() + "%'")[0][0]
+    except:
+        countContact=0
+
     count = countCustomer + countContact + countSupplier + 1
     #frappe.msgprint(count)
     series = prefix.upper() + '{:03d}'.format(count)
 
     while 1:
-        countCustomer = frappe.db.sql("select count(`name`) as n from `tabCustomer` where name = '" + series + "'")[0][0]
-        countSupplier = frappe.db.sql("select count(`name`) as n from `tabSupplier` where name = '" + series + "'")[0][0]
-        countContact = frappe.db.sql("select count(`name`) as n from `tabContact` where name = '" + series + "'")[0][0]
+        try:
+            countCustomer = frappe.db.sql("select count(`name`) as n from `tabCustomer` where name = '" + series + "'")[0][0]
+        except:
+            countCustomer =0
 
-        if countCustomer + countSupplier + countContact != 0:
+        try:
+            countSupplier = frappe.db.sql("select count(`name`) as n from `tabSupplier` where name = '" + series + "'")[0][0]
+        except:
+            countSupplier =0
+
+        try:
+            countContact = frappe.db.sql("select count(`name`) as n from `tabContact` where name = '" + series + "'")[0][0]
+        except:
+            countContact=0
+
+
+        if (countCustomer + countSupplier + countContact) != 0:
             count = count + 1
             #frappe.msgprint(count)
             series = prefix.upper() + '{:03d}'.format(count)
